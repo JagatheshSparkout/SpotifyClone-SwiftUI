6747342fe9f5f912cd54fb577cb876403f75e86a
JagatheshSparkout <jkaninfotech@gmail.com>
2020-08-17 00:24:24 +0530

    Auth screen design added

---
+++ Spotify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spotify
+//
+//  Created by Jack on 16/08/20.
+//  Copyright © 2020 xyz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
